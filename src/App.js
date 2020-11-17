905e3b103d811e6ec76b09f77392c9b042fd12b6
MatiMartinez <matia.martine2018@gmail.com>
2020-11-17 17:44:59 -0300

    feat: Chat Form
    Chat view, message form
    Styles without responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Chat from "./components/Chat/Chat";
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 
 export default function Chat({ location }) {
   const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const endpoint = "localhost:4000";
 
   useEffect(() => {
@@ -16,11 +18,29 @@ export default function Chat({ location }) {
     socket = io(endpoint);
     socket.emit("join", { name }, () => {});
 
-    return () => {
+    /*return () => {
       socket.emit("disconnect");
       socket.off();
-    };
+    };*/
   }, [endpoint, location.search]);
 
-  return <div>{name}</div>;
+  useEffect(() => {
+    socket.on("message", (msg) => {
+      setMessages([...messages], msg);
+    });
+  }, [messages]);
+
+  return (
+    <div className="main-container">
+      <div className="chat-container">
+        <div className="chat-content">
+          <div className="message"></div>
+        </div>
+        <div className="form-send">
+          <input type="text" onChange={(e) => setMessage(e.target.value)} />
+          <button>SEND</button>
+        </div>
+      </div>
+    </div>
+  );
 }
